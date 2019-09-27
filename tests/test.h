fb0bfcaf64ec760febdca86053797e3ac042f20a
fourtf <tf.four@gmail.com>
2019-09-27 20:08:00 +0200

    tests can now handle segfaults and div by 0

---
+++ tests/test.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <setjmp.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+// Recovery from segfault and other errors.
+jmp_buf failure_buffer;
+bool in_test = false;
+const char *signal_name = "";
+
+// Check if all tests passed.
+bool all_success = true;
+
+// Custom handler for segfault and other errors.
+void handle_signal(int signal) {
+  if (in_test) {
+    switch (signal) {
+    case SIGSEGV:
+      signal_name = "SIGSEGV";
+      break;
+    case SIGABRT:
+      signal_name = "SIGABRT";
+      break;
+    case SIGFPE:
+      signal_name = "SIGFPE";
+      break;
+    default:
+      signal_name = "UNKNOWN";
+      break;
+    }
+
+    longjmp(failure_buffer, 0);
+  }
+}
+
+// Runs a test and recovers from segfaults.
+void test(void (*test)(), const char *name) {
+  setjmp(failure_buffer);
+
+  if (in_test) {
+    printf("%s signal in '%s'.\n", signal_name, name);
+    all_success = false;
+  } else {
+    in_test = true;
+    test();
+  }
+
+  in_test = false;
+}
+
+void setup_tests() {
+  signal(SIGSEGV, handle_signal);
+  signal(SIGABRT, handle_signal);
+  signal(SIGFPE, handle_signal);
+}
+
+#define TEST_IMPL(a, b) test(&a, b)
+#define TEST(func) TEST_IMPL(func, #func)
+
+#define MUST_EQUAL(a, b)                                                       \
+  printf("%s: %s", __func__,                                                   \
+         (a == b ? "equal" : (all_success = false, "not equal")))
--- tests/tests.c
+++ tests/tests.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
+#include "test.h"
 
 // Ad hoc main for tests.
 int main(void) {
-  puts("All tests passed PogChamp");
-  return 0;
+  puts("Tests started\n");
+
+  setup_tests();
+
+  if (all_success) {
+    puts("All tests passed PogChamp");
+  } else {
+    puts("Some tests didn't pass :(");
+  }
+
+  return all_success ? 0 : 1;
 }
